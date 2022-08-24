147cdae930786d600cdbc5e85f82ed68a2efed16
Thoufiqhussain-DevQA <85802921+Thoufiqhussain-DevQA@users.noreply.github.com>
2022-08-24 21:56:55 +0530

    Payment-1

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO card_details ( s_No,card_Number, cvv,card_Holder_Name) VALUES
+  (1,7012345678910117,777,'user'),
+  (2,7012345678910118,888,'user'),
+  (3,7012345678910116,666,'user'),
+  (4,7012345678910115,555,'user');
+  
